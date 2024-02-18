5277f72d4d4499d59b8c37ecd48bc06f26e31053
Kade <kshockey@msudenver.edu>
2024-02-17 21:32:44 -0700

    video 1 so far

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from "./board/board.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SquareComponent, BoardComponent]
 })
 export class AppComponent {
   title = 'myapp';
