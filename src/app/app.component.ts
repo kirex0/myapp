29f27f20b8b2be10f10a0069abbf3070b6a64dec
Kade <kshockey@msudenver.edu>
2024-02-20 10:29:58 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SquareComponent } from "./square/square.component";
 import { BoardComponent } from "./board/board.component";
+import {  NbButtonModule, NbLayoutModule } from '@nebular/theme';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, SquareComponent, BoardComponent]
+    imports: [RouterOutlet, SquareComponent, BoardComponent, NbButtonModule],
+    providers: []
 })
 export class AppComponent {
   title = 'myapp';
